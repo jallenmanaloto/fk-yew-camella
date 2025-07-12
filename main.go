2abefd3ab6b3f328f54d8a5f5f2156cde93788eb
alleeeeenm <jallen.manaloto@gmail.com>
2025-07-12 12:44:27 +0800

    feat: integrate packages to run scheduled mailer

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/fk-yew-camella/config"
+	"github.com/fk-yew-camella/generator"
+	"github.com/fk-yew-camella/mailer"
+)
+
+func main() {
+	config, err := config.Load()
+	if err != nil {
+		panic(err) // what's the use if we can't load the config file, right?
+	}
+
+	// Do not run the app
+	if !config.Enable {
+		return
+	}
+
+	// Generate github workflow to run the app on schedule
+	cron := config.CronExpression()
+	generator.ScheduledWorkflow(cron)
+
+	// Initialize mailer and send the email
+	m := mailer.New(
+		"smtp.gmail.com",
+		"587",
+		config.Email,
+		config.Password,
+	)
+	m.Send(config)
+}
